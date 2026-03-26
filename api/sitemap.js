1acccb274901f0bbb33395c7e678ac14c66392fc
firefoxmcxd009-ops <firefoxmcxd009@gmail.com>
2026-03-26 19:26:34 +0700

    Create sitemap.js

---
+++ api/sitemap.js
@@ -0,0 +1,23 @@
+export default function handler(req, res) {
+const baseUrl = "https://foxmcstatus.vercel.app";
+
+const pages = [
+"/",                 // Home (index.html)
+"/Tools/Status",
+"/Tools/Skins",
+"/Tools/Bot",
+"/Socials"
+];
+
+const sitemap = "<?xml version="1.0" encoding="UTF-8"?> <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> ${pages.map((page) => "
+<url>
+<loc>${baseUrl}${page}</loc>
+<lastmod>${new Date().toISOString()}</lastmod>
+<changefreq>daily</changefreq>
+<priority>${page === "/" ? "1.0" : "0.8"}</priority>
+</url>
+").join("")} </urlset>";
+
+res.setHeader("Content-Type", "text/xml");
+res.status(200).send(sitemap);
+  }
