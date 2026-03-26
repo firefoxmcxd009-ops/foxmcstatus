a4fbc54ec5e24a94ce5bf028e2f698806cc45b00
firefoxmcxd009-ops <firefoxmcxd009@gmail.com>
2026-03-26 20:10:03 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -5,8 +5,7 @@ const pages = [
 "/",                 // Home (index.html)
 "/Tools/Status",
 "/Tools/Skins",
-"/Tools/Bot",
-"/Socials"
+"/Tools/Bot"
 ];
 
 const sitemap = "<?xml version="1.0" encoding="UTF-8"?> <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> ${pages.map((page) => "
