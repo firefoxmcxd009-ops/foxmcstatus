7b894efa7bf5e4b6af43e5213ca41e412535d42d
firefoxmcxd009-ops <firefoxmcxd009@gmail.com>
2026-03-26 16:42:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+async function getStatus() {
+    let ip = document.getElementById("ip").value;
+
+    let loader = document.getElementById("loader");
+    let result = document.getElementById("result");
+
+    loader.style.display = "block";
+    result.style.display = "none";
+
+    try {
+        let res = await fetch(`https://api.mcsrvstat.us/2/${ip}`);
+        let data = await res.json();
+
+        loader.style.display = "none";
+
+        if (data.online) {
+            result.style.display = "block";
+
+            document.getElementById("serverIP").innerText = ip;
+            document.getElementById("version").innerText = "Version: " + data.version;
+            document.getElementById("players").innerText = "Players: " + data.players.online + "/" + data.players.max;
+
+            if (data.icon) {
+                document.getElementById("icon").src = data.icon;
+            }
+        } else {
+            alert("Server Offline ❌");
+        }
+
+    } catch (error) {
+        loader.style.display = "none";
+        alert("Error fetching server ⚠️!\nCheck internet connection and try again :)");
+    }
+
+}
